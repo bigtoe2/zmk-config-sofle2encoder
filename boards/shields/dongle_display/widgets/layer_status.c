5f8f9ba183811a05ca59ae873fc2474f1cd109f5
Tony Hang <92767070+bigtoe2@users.noreply.github.com>
2025-09-04 12:37:25 -0500

    declare font

--- boards/shields/dongle_display/widgets/layer_status.c
+++ boards/shields/dongle_display/widgets/layer_status.c
@@ -15,6 +15,8 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #include <zmk/endpoints.h>
 #include <zmk/keymap.h>
 
+LV_FONT_DECLARE(LV_FONT_MONTSERRAT_40)
+
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
 struct layer_status_state {
